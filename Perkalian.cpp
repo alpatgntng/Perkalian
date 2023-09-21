03da8b9c2d8dddc3af458fbbb86432342c8ed2c2
alpatgntng <145646298+alpatgntng@users.noreply.github.com>
2023-09-20 18:58:29 -1200

    Create Perkalian.cpp

---
+++ Perkalian.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int a, b, c;
+    cout<< "Program Penjumlahan \n";
+     cout<<"Masukkan Angka pertama: ";
+     cin>>a;
+     cout<<"Masukan Angka kedua: ";
+     cin>>b;
+     cout<<"Hasil perkalian adalah: "<<a*b<<endl;
+     return 0;
+}
